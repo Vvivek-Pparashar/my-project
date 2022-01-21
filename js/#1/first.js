c98b848d4b05cc759b549af16636202e00f13d1e
Vvivek <vivekparashartkd@gmail.com>
2022-01-22 00:49:26 +0530

    first time deploy my work on git

---
+++ js/#1/first.js
@@ -0,0 +1,7 @@
+let ul =   document.getElementById('this');
+ul.removeChild(document.getElementById('yes1'));
+console.log(tnode);
+
+let myul = document.getElementById('myul');
+myul.replaceChild(element, document.getElementById('fui'));
+myul.removeChild(document.getElementById('lui'));
