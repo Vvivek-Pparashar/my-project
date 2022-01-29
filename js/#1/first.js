7b490120c24c72abf52b421aa0a67e812cea31c0
Vvivek <vivekparashartkd@gmail.com>
2022-01-29 21:15:10 +0530

    updated after 2 project in js

--- js/#1/first.js
+++
@@ -1,7 +0,0 @@
-let ul =   document.getElementById('this');
-ul.removeChild(document.getElementById('yes1'));
-console.log(tnode);
-
-let myul = document.getElementById('myul');
-myul.replaceChild(element, document.getElementById('fui'));
-myul.removeChild(document.getElementById('lui'));
