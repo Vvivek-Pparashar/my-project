ee6a5850b4ba4477e384845cf1a5417fc5890bec
Vvivek-Pparashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-08-24 18:38:57 +0530

    updated

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -6,55 +6,64 @@ export default function TextForm(props) {
   const handleLpClick = () => setText(text.toLowerCase());
   // const totalSpace = ()=> ()
 
-  const convertfirstletter = ()=>{
-    let arr = text.trim().split(/\s+/).filter(tag => tag.trim() !=="")
+  const convertfirstletter = () => {
+    let arr = text
+      .trim()
+      .split(/\s+/)
+      .filter((tag) => tag.trim() !== "");
 
     let newtext = "";
 
-        arr.forEach(e =>{
-             newtext += " " + e[0].toUpperCase() + e.substring(1)
-        })
+    arr.forEach((e) => {
+      newtext += " " + e[0].toUpperCase() + e.substring(1);
+    });
 
-      setText(newtext.trim());
-    }
+    setText(newtext.trim());
+  };
 
   const clearText = () => setText("");
-
 
   const [text, setText] = useState("");
 
   return (
     <>
-    <div>
-      <h1>{props.heading} </h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="Mybox"
-          rows="8"
-          placeholder="Type here"
-          value={text}
-          onChange={change}
-        ></textarea>
+      <div>
+        <h1>{props.heading} </h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="Mybox"
+            rows="8"
+            placeholder="Type here"
+            value={text}
+            onChange={change}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary me-4" onClick={handleUpClick}>
+          Convert To UpperCase
+        </button>
+        <button className="btn btn-primary me-4" onClick={handleLpClick}>
+          Convert To LowerCase
+        </button>
+        <button className="btn btn-primary me-4" onClick={convertfirstletter}>
+          Convert 1st letter To upperCase
+        </button>
+        <button className="btn btn-primary me-4" onClick={clearText}>
+          Clear Text
+        </button>
       </div>
-      <button className="btn btn-primary me-4" onClick={handleUpClick}>
-        Convert To UpperCase
-      </button>
-      <button className="btn btn-primary me-4" onClick={handleLpClick}>
-        Convert To LowerCase
-      </button>
-      <button className="btn btn-primary me-4" onClick={convertfirstletter}>
-        Convert 1st letter To upperCase
-      </button>
-      <button className="btn btn-primary me-4" onClick={clearText}>
-        Clear Text
-      </button>
 
-    </div>
-
-    <div className="container my-5">
-      <h1>Your text summary</h1>
-      <p>{text.trim().split(/\s+/).filter(tag => tag.trim() !=="").length} words & {text.length} words</p>
+      <div className="container my-5">
+        <h1>Your text summary</h1>
+        <p>
+          {
+            text
+              .trim()
+              .split(/\s+/)
+              .filter((tag) => tag.trim() !== "").length
+          }{" "}
+          words & {text.length} words
+        </p>
       </div>
     </>
   );
--- react/react 15 project/03-reviews/setup/src/index.js
+++ react/react 15 project/03-reviews/setup/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Facial.css";
+import Facial from "./Facial.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Facial />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
