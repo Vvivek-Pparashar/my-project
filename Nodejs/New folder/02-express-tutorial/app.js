efe3f232120e683d8e583b06b7406fa31b657f96
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2022-11-22 21:19:54 +0530

    vivek is gr8

--- Nodejs/New folder/02-express-tutorial/app.js
+++ Nodejs/New folder/02-express-tutorial/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static('./public'))
+
+app.get("/", (req, res)=>{
+  console.log(req.url);
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))  
+
+})
+
+app.all("*", (req, res)=>{
+  console.log(req.url);
+  res.status(404).send("fuck you 🤜🤛")
+})
 
 app.listen(5000, ()=>{
   console.log("vivek is gr8")
